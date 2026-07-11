3cf54ff2310c66bbfac50fa73557a8d5d3813e2e
Nacho Nawrath <inawrath@gmail.com>
2026-07-10 23:22:13 -0400

    chore: initial project structure for tmarea-backend

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import express from 'express'; import cors from 'cors'; const app = express(); app.use(cors()); app.get('/health', (req, res) => res.json({ status: 'ok' })); app.listen(3000, () => console.log('Backend running on port 3000'));
